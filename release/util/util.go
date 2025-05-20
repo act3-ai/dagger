364e11c084393e5c4e5b3146beda1f3a6d8adfd1
nathan-joslin <nathan.joslin@udri.udayton.edu>
2025-05-20 11:38:28 -0400

    remove unused project type resolution

--- release/util/util.go
+++ release/util/util.go
@@ -1,39 +1,12 @@
 package util
 
 import (
-	"errors"
 	"fmt"
 	"slices"
 	"strings"
 
 	"golang.org/x/mod/semver"
 )
-
-// Project is a programming language type.
-type ProjectType = string
-
-const (
-	Golang ProjectType = "go"
-	Python ProjectType = "python"
-)
-
-// ErrUnsupportedProject indicates a project's language is not supported.
-var ErrUnsupportedProject = errors.New("unsupported project type")
-
-// ResolveProjectType determines the type of a project, returning a standard
-// projecty type identifier or an error if it's not supported.
-func ResolveProjectType(language string) (ProjectType, error) {
-	lang := strings.ToLower(language)
-
-	switch lang {
-	case "golang", "go":
-		return Golang, nil
-	case "python", "py":
-		return Python, nil
-	default:
-		return "", fmt.Errorf("%w: recieved type %s", ErrUnsupportedProject, language)
-	}
-}
 
 // ResultsFormatter provides utility for formatting sets of results.
 //
