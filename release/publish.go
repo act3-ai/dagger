5206e0a2f03b0d33654b1044355203a5ad2961db
nathan-joslin <nathan.joslin@udri.udayton.edu>
2025-05-22 11:08:30 -0400

    mod(release): add support for releasing to gitlab or github

--- release/publish.go
+++ release/publish.go
@@ -2,9 +2,21 @@ package main
 
 import (
 	"context"
+	"dagger/release/internal/dagger"
 	"dagger/release/util"
 	"fmt"
 	"strings"
+
+	"github.com/sourcegraph/conc/pool"
+)
+
+// This file hosts general publish functions used by project specific publish methods.
+
+const (
+	imageOras = "ghcr.io/oras-project/oras:v1.2.3"
+	// A dagger module does exist for glab, but doesn't support custom base images,includes the deprecated gitlab release CLI, and doesn't have first-class support for release assets.
+	// https://github.com/vbehar/daggerverse/tree/838974e23bf2afb192850103c6d76fe620f31afd/gitlab-cli
+	imageGlabCLI = "registry.gitlab.com/gitlab-org/cli:latest"
 )
 
 // Generate extra tags based on the provided target tag.
@@ -33,7 +45,7 @@ func (r *Release) existingOCITags(ctx context.Context,
 	ref string,
 ) ([]string, error) {
 	oras := dag.Container().
-		From("ghcr.io/oras-project/oras:v1.2.3").
+		From(imageOras).
 		File("/bin/oras")
 
 	out, err := dag.Wolfi().
@@ -48,4 +60,146 @@ func (r *Release) existingOCITags(ctx context.Context,
 	}
 
 	return strings.Fields(out), nil
+}
+
+// gitlabRelease creates a release in a public or private GitLab instance.
+func (r *Release) gitlabRelease(ctx context.Context,
+	// GitLab host
+	// +optional
+	// +default=gitlab.com
+	host string,
+	// GitLab project (repository)
+	project string,
+	// GitLab personal access token
+	token *dagger.Secret,
+	// Release version
+	version string,
+	// Release notes file
+	notes *dagger.File,
+	// Release title. Default: version
+	// +optional
+	title string,
+	// Release assets
+	// +optional
+	assets []*dagger.File,
+) (string, error) {
+	notesFileName, err := notes.Name(ctx)
+	if err != nil {
+		return "", err
+	}
+
+	hostRepo := fmt.Sprintf("%s/%s", host, project)
+	if title == "" {
+		title = version
+	}
+
+	_, err = dag.Container().
+		From(imageGlabCLI).
+		WithMountedFile(notesFileName, notes).
+		WithSecretVariable("GITLAB_TOKEN", token).
+		WithEnvVariable("GITLAB_HOST", host).
+		WithExec([]string{"glab", "release", "create",
+			"-R", project, // repository
+			"v" + version,                 // tag
+			"--name=" + version,           // title
+			"--notes-file", notesFileName, // description
+		}).
+		Stdout(ctx)
+	if err != nil {
+		return "", fmt.Errorf("publishing release to '%s': %w", hostRepo, err)
+	}
+
+	if len(assets) > 0 {
+		_, err := r.gitlabUploadAssets(ctx, host, project, token, version, assets)
+		if err != nil {
+			return "", fmt.Errorf("uploading release assets for '%s' release '%s': %w", hostRepo, version, err)
+		}
+	}
+
+	return fmt.Sprintf("Successfully published release to '%s'", hostRepo), nil
+}
+
+// gitlabUploadAssets publishes assets to an existing release tag to GitLab.
+func (r *Release) gitlabUploadAssets(ctx context.Context,
+	// GitLab host
+	host string,
+	// GitLab project (repository)
+	project string,
+	// GitLab personal access token
+	token *dagger.Secret,
+	// Release version
+	version string,
+	// Release assets
+	assets []*dagger.File,
+) (string, error) {
+	ctx, span := Tracer().Start(ctx, "Upload Assets")
+	defer span.End()
+
+	p := pool.NewWithResults[string]().WithContext(ctx)
+	for _, asset := range assets {
+		p.Go(func(ctx context.Context) (string, error) {
+			assetName, err := asset.Name(ctx)
+			if err != nil {
+				return "", err
+			}
+			_, err = dag.Container().
+				From(imageGlabCLI).
+				WithMountedFile(assetName, asset).
+				WithSecretVariable("GITLAB_TOKEN", token).
+				WithEnvVariable("GITLAB_HOST", host).
+				WithExec([]string{"glab", "release", "upload",
+					"-R", project,
+					"v" + version,
+					assetName},
+				).
+				Stdout(ctx)
+
+			if err != nil {
+				return "", fmt.Errorf("uploading release asset %s: %w", assetName, err)
+			}
+			return fmt.Sprintf("Asset Uploaded - %s", assetName), nil
+		})
+	}
+
+	result, err := p.Wait()
+	return strings.Join(result, "\n"), err
+}
+
+// githubRelease creates a release in GitHub.
+func (r *Release) githubRelease(ctx context.Context,
+	// GitHub project (repository)
+	project string,
+	// gitlab personal access token
+	token *dagger.Secret,
+	// Release version
+	version string,
+	// Release notes file
+	notes *dagger.File,
+	// Release title. Default: version
+	// +optional
+	title string,
+	// Release assets
+	// +optional
+	assets []*dagger.File,
+) (string, error) {
+	if title == "" {
+		title = version
+	}
+
+	err := dag.Gh(
+		dagger.GhOpts{
+			Token:  token,
+			Repo:   project,
+			Source: r.Source,
+		}).
+		Release().
+		Create(ctx, version, title,
+			dagger.GhReleaseCreateOpts{
+				NotesFile: notes,
+				Files:     assets,
+			})
+	if err != nil {
+		return "", fmt.Errorf("publishing release to 'github.com/%s': %w", project, err)
+	}
+	return fmt.Sprintf("Successfully published release to 'github.com/%s'", project), nil
 }
