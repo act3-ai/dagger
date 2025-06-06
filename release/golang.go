f432d3878b83c6abea3d4587521bd34da3c0c936
nathan-joslin <nathan.joslin@udri.udayton.edu>
2025-06-06 13:25:17 -0400

    feat(release): Add go verify to run gorelease on module version

--- release/golang.go
+++ release/golang.go
@@ -11,12 +11,19 @@ import (
 
 type Golang struct {
 	Release *Release
+
+	GoPrivate string
 }
 
 // Go provides utilities for releasing golang projects.
-func (r *Release) Go() *Golang {
+func (r *Release) Go(
+	// value of GOPRIVATE
+	// +optional
+	goPrivate string,
+) *Golang {
 	return &Golang{
-		Release: r,
+		Release:   r,
+		GoPrivate: goPrivate,
 	}
 }
 
@@ -60,16 +67,7 @@ func (g *Golang) Check(ctx context.Context,
 	}
 
 	// unit tests
-	// TODO: GOPRIVATE?
-	res, err = dag.Go(dagger.GoOpts{Container: unitTestBase}).
-		WithSource(g.Release.Source).
-		Container().
-		With(func(ctr *dagger.Container) *dagger.Container {
-			if g.Release.Netrc != nil {
-				ctr = ctr.WithMountedSecret("/root/.netrc", g.Release.Netrc)
-			}
-			return ctr
-		}).
+	res, err = g.goContainer(unitTestBase).
 		WithExec([]string{"go", "test", "./..."}).
 		Stdout(ctx)
 	results.Add("Go Unit Tests", res)
@@ -82,4 +80,66 @@ func (g *Golang) Check(ctx context.Context,
 	}
 
 	return results.String(), errors.Join(errs...)
+}
+
+// Verify release version adheres to gorelease standards.
+//
+// See https://pkg.go.dev/golang.org/x/exp/cmd/gorelease.
+func (g *Golang) Verify(ctx context.Context,
+	// target module version.
+	targetVersion string,
+	// current module version. Default: contents of VERSION file, with 'v' prefix added.
+	// +optional
+	currentVersion string,
+	// base container.
+	// +optional
+	base *dagger.Container,
+) error {
+	const gorelease = "golang.org/x/exp/cmd/gorelease@latest"
+
+	var err error
+	if currentVersion == "" {
+		currentVersion, err = g.Release.Source.File("VERSION").Contents(ctx)
+		if err != nil {
+			return fmt.Errorf("retreving version from VERSION file: %w", err)
+		}
+	}
+
+	if !strings.HasPrefix("v", currentVersion) {
+		currentVersion = "v" + currentVersion
+	}
+
+	_, err = g.goContainer(nil).
+		WithExec([]string{"go", "install", gorelease}).
+		WithExec([]string{"/work/src/tool/gorelease", fmt.Sprintf("-base=%s", currentVersion), fmt.Sprintf("-version=%s", targetVersion)}).
+		Stdout(ctx)
+
+	var e *dagger.ExecError
+	switch {
+	case errors.As(err, &e):
+		// exit code != 0
+		return fmt.Errorf("%s", e.Stderr)
+	case err != nil:
+		// some other dagger error, e.g. graphql
+		return err
+	default:
+		// exit code = 0
+		return nil
+	}
+}
+
+// goContainer returns a go container with private auth setup, if applicable.
+func (g *Golang) goContainer(base *dagger.Container) *dagger.Container {
+	return dag.Go(dagger.GoOpts{Container: base}).
+		WithSource(g.Release.Source).
+		WithEnvVariable("GOBIN", "/work/src/tool").
+		WithEnvVariable("GOPRIVATE", g.GoPrivate).
+		Container().
+		With(func(ctr *dagger.Container) *dagger.Container {
+			if g.Release.Netrc != nil {
+				ctr = ctr.WithMountedSecret("/root/.netrc", g.Release.Netrc)
+			}
+			return ctr
+		}).
+		WithMountedCache("/go/pkg/mod", dag.CacheVolume("go-mod"))
 }
