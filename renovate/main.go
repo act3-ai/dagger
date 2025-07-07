040b96eb79e025924e3222b56f9429f6507ad462
Paul Spurlock <pspurlock07@gmail.com>
2025-07-07 14:15:28 -0400

    fix: update renovate to 41.23.5-full

--- renovate/main.go
+++ renovate/main.go
@@ -114,7 +114,7 @@ func New(
 	enabledManagers string,
 ) *Renovate {
 	if base == nil {
-		base = dag.Container().From("renovate/renovate:41.1.3-full")
+		base = dag.Container().From("renovate/renovate:41.23.5-full")
 	}
 	return &Renovate{
 		Project:         project,
