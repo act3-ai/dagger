0177d9436b41399de2338d8a9e6781bb5c54d7f8
Paul Spurlock <pspurlock07@gmail.com>
2025-07-16 18:30:16 -0400

    fix: add option for additional build args in publish

--- python/publish.go
+++ python/publish.go
@@ -7,21 +7,23 @@ import (
 
 // publish python package/wheel
 func (python *Python) Publish(ctx context.Context,
+	// extra args to pass into uv build
+	// +optional
+	buildArgs []string,
 	publishUrl string,
 	username string,
 	password *dagger.Secret,
 ) (*dagger.Container, error) {
+
+	buildCmd := []string{"uv", "build"}
+	buildCmd = append(buildCmd, buildArgs...)
 
 	c := python.Container().
 		WithEnvVariable("UV_PUBLISH_CHECK_URL", publishUrl+"/simple").
 		WithEnvVariable("UV_PUBLISH_URL", publishUrl).
 		WithEnvVariable("UV_PUBLISH_USERNAME", username).
 		WithSecretVariable("UV_PUBLISH_PASSWORD", password).
-		WithExec(
-			[]string{
-				"uv",
-				"build",
-			}).
+		WithExec(buildCmd).
 		WithExec(
 			[]string{
 				"uv",
