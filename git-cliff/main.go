730de2ded4773b679f4d205bd8cbe38bc57d1932
Paul Spurlock <pspurlock07@gmail.com>
2025-11-05 10:19:24 -0500

    fix: move config out of New and into functions instead

--- git-cliff/main.go
+++ git-cliff/main.go
@@ -32,10 +32,6 @@ func New(ctx context.Context,
 	// +default="latest"
 	version string,
 
-	// Configuration file path in provided git repository/ref.
-	// +optional
-	config string,
-
 	// Mount netrc credentials for a private git repository.
 	// +optional
 	netrc *dagger.Secret,
@@ -43,32 +39,18 @@ func New(ctx context.Context,
 	if container == nil {
 		container = defaultContainer(version)
 	}
-
+	gitRefDir := gitref.Tree(dagger.GitRefTreeOpts{Depth: -1})
 	cmd := []string{"git-cliff", "--use-native-tls"}
 	srcDir := "/work/src"
 	container = container.With(
-		func(c *dagger.Container) *dagger.Container {
-			if config != "" {
-				exists, err := gitref.Tree(dagger.GitRefTreeOpts{Depth: -1}).Exists(ctx, config)
-
-				if err != nil {
-					panic(fmt.Errorf("resolving configuration file name: %w", err))
-				} else if !exists {
-					panic(fmt.Errorf("configuration file not found: %w", err))
-				} else {
-					cmd = append(cmd, "--config", config)
-				}
-			}
-			return c
-		}).With(
 		func(c *dagger.Container) *dagger.Container {
 			if netrc != nil {
 				c = c.WithMountedSecret("/root/.netrc", netrc)
 			}
 			return c
 		}).
-		WithWorkdir(srcDir).
-		WithMountedDirectory(srcDir, gitref.Tree(dagger.GitRefTreeOpts{Depth: -1}))
+		WithMountedDirectory(srcDir, gitRefDir).
+		WithWorkdir(srcDir)
 
 	return &GitCliff{
 		Container: container,
@@ -149,6 +131,15 @@ func (gc *GitCliff) WithSecretVariable(
 	return gc
 }
 
+func (gc *GitCliff) WithConfig(
+	// Configuration file path in provided git repository/ref.
+	// +optional
+	config string,
+) *GitCliff {
+	gc.Command = append(gc.Command, "--config", config)
+	return gc
+}
+
 // Run git-cliff with all options previously provided.
 //
 // Run MAY be used as a "catch-all" in case functions are not implemented.
@@ -164,6 +155,9 @@ func (gc *GitCliff) Run(
 
 // Prints bumped version for unreleased changes.
 func (gc *GitCliff) BumpedVersion(ctx context.Context,
+	// Configuration file path in provided git repository/ref.
+	// +optional
+	config string,
 	// additional arguments and flags for git-cliff
 	// +optional
 	args []string,
@@ -171,6 +165,10 @@ func (gc *GitCliff) BumpedVersion(ctx context.Context,
 	cmd := gc.Command
 	cmd = append(cmd, "--bumped-version")
 	cmd = append(cmd, args...)
+
+	if config != "" {
+		cmd = append(cmd, "--config", config)
+	}
 
 	ctr := gc.Container.WithExec(cmd)
 	// The check below is needed due to how git-cliff returns its warning/error logs.
