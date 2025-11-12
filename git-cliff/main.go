7c1937b62c5c9ed988931d044c948eb910cea1df
Paul Spurlock <pspurlock07@gmail.com>
2025-11-12 17:02:23 -0500

    refactor: create generate changelog and notes functions

--- git-cliff/main.go
+++ git-cliff/main.go
@@ -13,34 +13,11 @@ const (
 )
 
 type GitCliff struct {
+	Container *dagger.Container
 	// +private
 	Gitref *dagger.GitRef
 	// +private
-	Options *Options
-}
-
-// Options represents all configurable options for running git-cliff
-type Options struct {
-	GitCliffVersion string
-	Netrc           *dagger.Secret
-	Tag             string
-	TagPattern      []string
-	Bump            bool
-	BumpedVersion   bool
-	Unreleased      bool
-	Strip           string
-	OutputFile      string
-	Prepend         string
-	GithubToken     *dagger.Secret
-	GitlabToken     *dagger.Secret
-	GiteaToken      *dagger.Secret
-	Config          string
-	IncludePath     []string
-	ExcludePath     []string
-	SkipCommits     []string
-	Latest          bool
-	Current         bool
-	TopoOrder       bool
+	Command []string
 }
 
 func New(ctx context.Context,
@@ -50,34 +27,6 @@ func New(ctx context.Context,
 	// +optional
 	// +default=latest
 	gitCliffVersion string,
-	// Mount netrc credentials.
-	// +optional
-	netrc *dagger.Secret,
-	// Sets the tag for the latest version
-	// +optional
-	tag string,
-	// Sets the regex for matching git tags
-	// +optional
-	tagPattern []string,
-	// Bumps the version for unreleased changes. Optionally with specified version
-	// +optional
-	bump bool,
-	// Prints bumped version for unreleased changes
-	// +optional
-	bumpedVersion bool,
-	//processes the commits that do not belong to a tag
-	// +optional
-	// +default=true
-	unreleased bool,
-	// Strips the given parts from the changelog [possible values: header, footer, all]
-	// +optional
-	strip string,
-	//writes output to the given file
-	// +optional
-	outputFile string,
-	//Prepends entries to the given changelog file
-	// +optional
-	prepend string,
 	// private token to use when authenticating with a private github instance set in cliff.toml
 	// See: https://git-cliff.org/docs/integration/github
 	// +optional
@@ -90,167 +39,132 @@ func New(ctx context.Context,
 	// See: https://git-cliff.org/docs/integration/gitea
 	// +optional
 	giteaToken *dagger.Secret,
-	// path to a git-cliff config. Defaults to root directory of given git-ref source
-	// +optional
-	config string,
-	// Sets the path to include related commits
-	// +optional
-	includePath []string,
-	// Sets the path to exnclude related commits
-	// +optional
-	excludePath []string,
-	// Sets commits that will be skipped in the changelog
-	// +optional
-	skipCommits []string,
-	// Processes the commits starting from the latest tag
-	// +optional
-	latest bool,
-	// Processes the commits that belong to the current tag
-	// +optional
-	current bool,
-	// Sorts the tags topologically
-	// +optional
-	topoOrder bool,
 ) *GitCliff {
-	return &GitCliff{
-		Gitref: gitref,
-		Options: &Options{
-			GitCliffVersion: gitCliffVersion,
-			Netrc:           netrc,
-			Tag:             tag,
-			TagPattern:      tagPattern,
-			Bump:            bump,
-			BumpedVersion:   bumpedVersion,
-			Unreleased:      unreleased,
-			Strip:           strip,
-			OutputFile:      outputFile,
-			Prepend:         prepend,
-			GithubToken:     githubToken,
-			GitlabToken:     gitlabToken,
-			GiteaToken:      giteaToken,
-			Config:          config,
-			IncludePath:     includePath,
-			ExcludePath:     excludePath,
-			SkipCommits:     skipCommits,
-			Latest:          latest,
-			Current:         current,
-			TopoOrder:       topoOrder,
-		},
-	}
-}
 
-// Run git-cliff with all options provided.
-//
-// Run MAY be used as a "catch-all" in case functions are not implemented.
-func (gc *GitCliff) Run() *dagger.Container {
 	//convert git-ref to a *dagger.Directory
-	gitRefDir := gc.Gitref.Tree(dagger.GitRefTreeOpts{Depth: -1})
+	gitRefDir := gitref.Tree(dagger.GitRefTreeOpts{Depth: -1})
 	//default git-cliff cmd
 	cmd := []string{"git-cliff", "--use-native-tls"}
 	srcDir := "/work/src"
 	//base git-cliff container
 	ctr := dag.Container().
-		From(fmt.Sprintf("%s:%s", imageGitCliff, gc.Options.GitCliffVersion)).
+		From(fmt.Sprintf("%s:%s", imageGitCliff, gitCliffVersion)).
 		WithMountedDirectory(srcDir, gitRefDir).
 		WithWorkdir(srcDir)
 
-	// parse given options set
-	if gc.Options.Netrc != nil {
-		ctr = ctr.WithMountedSecret("/root/.netrc", gc.Options.Netrc)
+	if githubToken != nil {
+		ctr = ctr.
+			WithSecretVariable("GITLAB_TOKEN", githubToken)
 	}
 
-	if gc.Options.Tag != "" {
-		cmd = append(cmd, "--tag", gc.Options.Tag)
+	if gitlabToken != nil {
+		ctr = ctr.
+			WithSecretVariable("GITLAB_TOKEN", gitlabToken)
+	}
+	if giteaToken != nil {
+		ctr = ctr.
+			WithSecretVariable("GITEA_TOKEN", giteaToken)
 	}
 
-	if len(gc.Options.TagPattern) > 0 {
-		cmd = append(cmd, "--tag-pattern", strings.Join(gc.Options.TagPattern, ","))
+	return &GitCliff{
+		Container: ctr,
+		Command:   cmd,
+		Gitref:    gitref,
+	}
+}
+
+// generate a changelog file with unreleased changes and bumps the tag
+// If file already exists, it will prepend to the existing changelog instead of creating a new one.
+func (gc *GitCliff) GenerateChangelog(
+	ctx context.Context,
+	//file path to output or prepend generated changelog.
+	// +optional
+	// +default="CHANGELOG.md"
+	changelog string,
+	//tag to generate changelog for
+	// +optional
+	tag string,
+	//path to git-cliff config to use for generating changelog in provided git-ref source.
+	// +optional
+	// +default="cliff.toml"
+	config string,
+) *dagger.File {
+	cmd := gc.Command
+	cmd = append(cmd,
+		"--config", config,
+		"--unreleased",
+		"--strip",
+		"footer",
+	)
+
+	//use provided tag, otherwise bump automatically
+	if tag != "" {
+		cmd = append(cmd, "--tag", tag)
+	} else {
+		cmd = append(cmd, "--bump")
+	}
+	//check if changelog exists and either prepend or generate new changelog file
+	exists, err := gc.Gitref.Tree().Exists(ctx, changelog)
+	if err != nil {
+		panic(fmt.Errorf("failed to check if %s exists: %w", changelog, err))
 	}
 
-	if gc.Options.Bump {
+	if exists {
+		cmd = append(cmd, "--prepend", changelog)
+	} else {
+		cmd = append(cmd, "--output", changelog)
+	}
+
+	return gc.Container.WithExec(cmd).File(changelog)
+}
+
+// generate release notes with unreleased changes and bumps the tag if tag is not provided
+func (gc *GitCliff) GenerateReleaseNotes(
+	ctx context.Context,
+	//file path to output release notes.
+	// +optional
+	// +default="releasenotes.md"
+	notes string,
+	//tag to generate changelog for
+	// +optional
+	tag string,
+	//path to git-cliff config to use for generating changelog in provided git-ref source.
+	// +optional
+	// +default="cliff.toml"
+	config string,
+) *dagger.File {
+	cmd := gc.Command
+	cmd = append(cmd,
+		"--config", config,
+		"--unreleased",
+		"--strip",
+		"all",
+		"--output", notes,
+	)
+
+	//use provided tag, otherwise bump automatically
+	if tag != "" {
+		cmd = append(cmd, "--tag", tag)
+	} else {
 		cmd = append(cmd, "--bump")
 	}
 
-	if gc.Options.BumpedVersion {
-		cmd = append(cmd, "--bumped-version")
-	}
-
-	if gc.Options.Unreleased {
-		cmd = append(cmd, "--unreleased")
-	}
-
-	if gc.Options.Strip != "" {
-		cmd = append(cmd, "--strip", gc.Options.Strip)
-	}
-
-	if gc.Options.OutputFile != "" {
-		cmd = append(cmd, "--output", gc.Options.OutputFile)
-	}
-
-	if gc.Options.Prepend != "" {
-		cmd = append(cmd, "--prepend", gc.Options.Prepend)
-	}
-
-	if gc.Options.GithubToken != nil {
-		ctr = ctr.
-			WithSecretVariable("GITHUB_TOKEN", gc.Options.GithubToken)
-	}
-	if gc.Options.GitlabToken != nil {
-		ctr = ctr.
-			WithSecretVariable("GITLAB_TOKEN", gc.Options.GitlabToken)
-	}
-	if gc.Options.GiteaToken != nil {
-		ctr = ctr.
-			WithSecretVariable("GITEA_TOKEN", gc.Options.GiteaToken)
-	}
-
-	if gc.Options.Config != "" {
-		cmd = append(cmd, "--config", gc.Options.Config)
-	}
-
-	if len(gc.Options.IncludePath) > 0 {
-		cmd = append(cmd, "--include-path", strings.Join(gc.Options.IncludePath, ","))
-	}
-
-	if len(gc.Options.ExcludePath) > 0 {
-		cmd = append(cmd, "--exclude-path", strings.Join(gc.Options.ExcludePath, ","))
-	}
-
-	if gc.Options.Latest {
-		cmd = append(cmd, "--latest")
-	}
-
-	if gc.Options.Current {
-		cmd = append(cmd, "--current")
-	}
-
-	if gc.Options.TopoOrder {
-		cmd = append(cmd, "--topo-order")
-	}
-
-	// cmd := gc.Command
-	// cmd = append(cmd, args...)
-	// return gc.Container.WithExec(cmd)
-
-	return ctr.WithExec(cmd)
+	return gc.Container.WithExec(cmd).File(notes)
 }
 
 // Prints bumped version for unreleased changes.
 func (gc *GitCliff) BumpedVersion(ctx context.Context,
-	// Configuration file path in provided git repository/ref.
+	//path to git-cliff config to use for generating changelog in provided git-ref source.
 	// +optional
+	// +default="cliff.toml"
 	config string,
-	// additional arguments and flags for git-cliff
-	// +optional
-	args []string,
 ) (string, error) {
 	cmd := gc.Command
-	cmd = append(cmd, "--bumped-version")
-	cmd = append(cmd, args...)
-
-	if config != "" {
-		cmd = append(cmd, "--config", config)
-	}
+	cmd = append(cmd,
+		"--config", config,
+		"--unreleased",
+		"--bumped-version",
+	)
 
 	ctr := gc.Container.WithExec(cmd)
 	// The check below is needed due to how git-cliff returns its warning/error logs.
