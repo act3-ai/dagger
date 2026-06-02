f7f16a75d45495c9bdbc231b35105aba7982f28b
Paul Spurlock <pspurlock07@gmail.com>
2026-06-02 16:14:08 -0400

    refactor

--- sonarqube/main.go
+++ sonarqube/main.go
@@ -14,23 +14,10 @@ type Sonarqube struct{}
 
 const hardcodedPassword = "MyHardcodedPass123!"
 
-func (m *Sonarqube) curlCtr(svc *dagger.Service) *dagger.Container {
+// start up sonar-server as a service
+func (m *Sonarqube) Service() *dagger.Service {
+
 	return dag.Container().
-		From("alpine:latest").
-		WithExec([]string{"apk", "add", "curl", "jq"}).
-		WithServiceBinding("sonar-server", svc).
-		WithEnvVariable("CACHEBUSTER", time.Now().String())
-}
-
-// start up sonar-server with the admin password changed and project created in sonar-server.
-func (m *Sonarqube) Service(ctx context.Context,
-	// name of project
-	projectName string,
-	// src directory to scan
-	src *dagger.Directory) (*dagger.Service, error) {
-	hardcodedPassword := "MyHardcodedPass123!"
-
-	sonarSvc := dag.Container().
 		From("sonarqube:community").
 		WithEnvVariable("SONAR_ES_BOOTSTRAP_CHECKS_DISABLE", "true").
 		WithExposedPort(9000).
@@ -46,11 +33,107 @@ func (m *Sonarqube) Service(ctx context.Context,
 				Retries:  15,
 			},
 		).AsService()
+}
 
-	curlCtr := m.curlCtr(sonarSvc)
+// scan a source directory with sonar-scanner and get a report from sonar-server
+func (m *Sonarqube) Scan(ctx context.Context,
+	projectName string,
+	src *dagger.Directory) (*dagger.File, error) {
+	//start sonar-server
+	sonarSvc, err := m.Service().Start(ctx)
 
+	if err != nil {
+		return nil, fmt.Errorf("failed to start sonar service: %w", err)
+	}
+
+	//setup admin pw/project
+	if err := m.serverSetup(ctx, sonarSvc, projectName); err != nil {
+		return nil, err
+	}
+
+	//generate sonar token
+	sonarToken, err := m.generateSonarToken(ctx, sonarSvc, projectName)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// run sonar scan
+	_, err = dag.Container().
+		From("sonarsource/sonar-scanner-cli:latest").
+		WithServiceBinding("sonar-server", sonarSvc).
+		WithDirectory("/src", src, dagger.ContainerWithDirectoryOpts{Owner: "scanner-cli"}).
+		WithWorkdir("/src").
+		WithSecretVariable("SONAR_TOKEN", sonarToken).
+		WithEnvVariable("SONAR_HOST_URL", "http://sonar-server:9000").
+		WithEnvVariable("CACHEBUSTER", time.Now().String()).
+		WithExec([]string{
+			"sonar-scanner",
+			"-Dsonar.projectName=" + projectName,
+			"-Dsonar.projectKey=" + projectName,
+			"-Dsonar.sources=.",
+			"-Dsonar.tests=.",
+			"-Dsonar.test.inclusions=src/**/*.test.*,src/**/*.spec.*",
+			"-Dsonar.exclusions=node_modules/**,dist/**,build/**,coverage/**,public/**,src/api/**",
+		}).Sync(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to run sonarqube scan: %w", err)
+	}
+
+	// get json report of issues
+	report := m.getReport(sonarSvc, sonarToken, projectName)
+
+	return report, nil
+}
+
+func (m *Sonarqube) curlCtr(svc *dagger.Service) *dagger.Container {
+	return dag.Container().
+		From("alpine:latest").
+		WithExec([]string{"apk", "add", "curl", "jq"}).
+		WithServiceBinding("sonar-server", svc).
+		WithEnvVariable("CACHEBUSTER", time.Now().String())
+}
+
+// create sonar project token to run a scan with
+func (m *Sonarqube) generateSonarToken(ctx context.Context, svc *dagger.Service, projectName string) (*dagger.Secret, error) {
+
+	token, err := m.curlCtr(svc).
+		WithExec([]string{"sh", "-c", fmt.Sprintf(`
+		curl -s -X POST -u admin:%s http://sonar-server:9000/api/user_tokens/generate \
+			-d "name=dagger-token" \
+			-d "type=PROJECT_ANALYSIS_TOKEN" \
+			-d "projectKey=%s" | jq -r '.token'
+	`, hardcodedPassword, projectName)}).Stdout(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to automatically create sonar token: %w", err)
+	}
+
+	return dag.SetSecret("SONAR_TOKEN", strings.TrimSpace(token)), nil
+
+}
+
+// get generated report in sonar
+func (m *Sonarqube) getReport(svc *dagger.Service, token *dagger.Secret, projectName string) *dagger.File {
+
+	return m.curlCtr(svc).
+		WithEnvVariable("PROJECT_NAME", projectName).
+		WithSecretVariable("SONAR_TOKEN", token).
+		WithExec([]string{"sh", "-c", "sleep 20"}). //HACK: Sonar takes time to build the report after scan, so we wait for it to finish
+		WithExec([]string{
+			"sh",
+			"-c",
+			`curl -u "$SONAR_TOKEN:" \
+      "http://sonar-server:9000/api/issues/search?componentKeys=$PROJECT_NAME"`,
+		}, dagger.ContainerWithExecOpts{RedirectStdout: "sonar-report.json"}).File("sonar-report.json")
+
+}
+
+// create initial admin password and create new project with project name
+func (m *Sonarqube) serverSetup(ctx context.Context, svc *dagger.Service, projectName string) error {
 	// change admin password on first run
-	_, err := curlCtr.
+	_, err := m.curlCtr(svc).
 		WithExec([]string{
 			"curl",
 			"-X", "POST",
@@ -63,11 +146,11 @@ func (m *Sonarqube) Service(ctx context.Context,
 		Sync(ctx)
 
 	if err != nil {
-		return nil, fmt.Errorf("failed to automatically change admin password: %w", err)
+		return fmt.Errorf("failed to automatically create admin password: %w", err)
 	}
 
-	// create project in sonarqube
-	_, err = curlCtr.
+	// create project in sonarqube with project name
+	_, err = m.curlCtr(svc).
 		WithExec([]string{
 			"curl",
 			"-X", "POST",
@@ -79,73 +162,9 @@ func (m *Sonarqube) Service(ctx context.Context,
 		Sync(ctx)
 
 	if err != nil {
-		return nil, fmt.Errorf("failed to automatically create project: %w", err)
+		return fmt.Errorf("failed to automatically create project: %w", err)
 	}
 
-	return sonarSvc, nil
-}
+	return nil
 
-// scan a source directory with sonar-scanner and get a report from sonar-server
-func (m *Sonarqube) Scan(ctx context.Context,
-	projectName string,
-	src *dagger.Directory) (*dagger.File, error) {
-	sonarSvc, err := m.Service(ctx, projectName, src)
-	if err != nil {
-		return nil, fmt.Errorf("failed to automatically create project: %w", err)
-	}
-
-	// create sonar token needed to scan with
-	projectToken, err := m.curlCtr(sonarSvc).
-		WithExec([]string{"sh", "-c", fmt.Sprintf(`
-		curl -s -X POST -u admin:%s http://sonar-server:9000/api/user_tokens/generate \
-			-d "name=dagger-token" \
-			-d "type=PROJECT_ANALYSIS_TOKEN" \
-			-d "projectKey=%s" | jq -r '.token'
-	`, hardcodedPassword, projectName)}).Stdout(ctx)
-
-	if err != nil {
-		return nil, fmt.Errorf("failed to obtain project token: %w", err)
-	}
-
-	projectToken = strings.TrimSpace(projectToken)
-	sonarToken := dag.SetSecret("SONAR_TOKEN", projectToken)
-
-	// run sonar scan
-	_, err = dag.Container().
-		From("sonarsource/sonar-scanner-cli:latest").
-		WithServiceBinding("sonar-server", sonarSvc).
-		WithDirectory("/src", src, dagger.ContainerWithDirectoryOpts{Owner: "scanner-cli"}).
-		WithWorkdir("/src").
-		WithSecretVariable("SONAR_TOKEN", sonarToken).
-		WithEnvVariable("SONAR_HOST_URL", "http://sonar-server:9000").
-		WithExec([]string{
-			"sonar-scanner",
-			"-Dsonar.projectName=" + projectName,
-			"-Dsonar.projectKey=" + projectName,
-			"-Dsonar.sources=.",
-			"-Dsonar.tests=.",
-			"-Dsonar.test.inclusions=src/**/*.test.*,src/**/*.spec.*",
-			"-Dsonar.exclusions=node_modules/**,dist/**,build/**,coverage/**,public/**,src/api/**",
-			// "-Dsonar.javascript.lcov.reportPaths=coverage/lcov.info",
-			// "-Dsonar.issue.ignore.multicriteria=e1",
-			// "-Dsonar.issue.ignore.multicriteria.e1.ruleKey=css:S4662",
-			// "-Dsonar.issue.ignore.multicriteria.e1.resourceKey=**/*.css",
-		}).Sync(ctx)
-
-	if err != nil {
-		return nil, fmt.Errorf("failed to run sonarqube scan: %w", err)
-	}
-
-	// get json report of issues
-	report := m.curlCtr(sonarSvc).
-		WithSecretVariable("SONAR_TOKEN", sonarToken).
-		// WithExec([]string{"sh", "-c", "sleep 20"}).
-		WithExec([]string{
-			"sh",
-			"-c",
-			`curl -u "$SONAR_TOKEN:" \
-      "http://sonar-server:9000/api/issues/search?componentKeys=paul"`,
-		}, dagger.ContainerWithExecOpts{RedirectStdout: "sonar-report.json"}).File("sonar-report.json")
-
-	return report, nil
 }
