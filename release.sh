aef4bae196286f3193722ac83f4458aa8f849def
Paul Spurlock <pspurlock07@gmail.com>
2026-06-22 14:07:09 -0400

    update release.sh

--- release.sh
+++ release.sh
@@ -50,7 +50,7 @@ prepare)
     git fetch --tags
 
     #run module tests
-    if [[ "$module" == "govulncheck" || "$module" == "renovate" ]]; then
+    if [[ "$module" == "govulncheck" || "$module" == "renovate" || "$module" == "soanrqube" ]]; then
       : # Do nothing
     else
       dagger -m "$module/tests" checks
